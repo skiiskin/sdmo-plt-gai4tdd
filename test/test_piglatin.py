2b77fa122990bf544a07f983c49f7632f39df70a
Satu Kiiskinen <satsu.kiiskinen@gmail.com>
2024-10-31 16:59:17 +0200

    US1 - T1 improved test name

--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -5,7 +5,7 @@ from error import PigLatinError
 
 class TestPigLatin(unittest.TestCase):
 
-    def test_translate_phrase(self):
+    def test_get_phrase(self):
         translator = PigLatin("hello world")
         self.assertEqual("hello world", translator.get_phrase())
 
